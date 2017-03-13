ca22abfc671b04e9f5ddf4ebe2ce7f80fc3a2885
Alex Baier <abaier@uni-koblenz.de>
2017-03-13 16:02:51 +0100

    Implemented/debugged piecewise linear projection classifier.

--- algorithm/classification.py
+++ algorithm/classification.py
@@ -67,7 +67,7 @@ class LinearProjectionClassifier(ProjectionClassifier):
         self.__phi = np.random.random((embedding_size, embedding_size))
 
     def train(self, training_data: List[Tuple[np.array, np.array]],
-              parts: int=8, tol: float=1e-3, eps: float= 1e-5, max_iter: int=500000):
+              parts: int=8, tol: float=1e-3, eps: float= 1e-5, max_iter: int=250000):
         n = len(training_data)  # number of samples
         if parts <= n:
             part_distribution = [int(n / parts) for _ in range(parts)]
@@ -76,6 +76,7 @@ class LinearProjectionClassifier(ProjectionClassifier):
             part_distribution = [int(n)]
 
         x, y = list(map(np.array, zip(*training_data)))
+        del training_data
 
         def f(phi):
             # minimize flattens input matrix, return it to matrix shape
@@ -98,12 +99,13 @@ class LinearProjectionClassifier(ProjectionClassifier):
         self.__phi = np.reshape(result.x, (self.__embedding_size, self.__embedding_size))
 
     def classify(self, unknowns: np.array)->List[str]:
+        labels = list()
         # project all unknowns
         y = unknowns @ self.__phi
         # calculate similarities between projections and all embeddings
         # get indices of most similar embedding
         indexes = np.argmax(cosine_similarity(y, self.__embeddings), axis=1)
-        labels = list()
+
         for index in indexes:
             labels.append(self.__labels[index])
         return labels
@@ -119,31 +121,56 @@ class PiecewiseLinearProjectionClassifier(ProjectionClassifier):
         self.__labels = labels  # type: List[str]
         self.__phi = [np.random.random((embedding_size, embedding_size)) for _ in range(clusters)]
 
-    def train(self, training_data: List[Tuple[np.array, np.array]]):
-        # TODO: debug
+    def train(self, training_data: List[Tuple[np.array, np.array]],
+              tol: float=1e-3, eps: float= 1e-5, max_iter: int=250000):
         n = len(training_data)  # number of samples
         self.__kmeans.fit(self.__embeddings)
+
+        x, y = list(map(np.array, zip(*training_data)))
+        del training_data
+
+        cluster_labels = self.__kmeans.predict(x)
+        clustered_x = [list() for _ in range(self.__clusters)]
+        clustered_y = [list() for _ in range(self.__clusters)]
+        for i, c in enumerate(cluster_labels):
+            clustered_x[c].append(x[i])
+            clustered_y[c].append(y[i])
+        del x
+        del y
+        clustered_x = list(map(np.array, clustered_x))
+        clustered_y = list(map(np.array, clustered_y))
+
         for k in range(self.__clusters):
 
             def f(phi):
                 # minimize flattens input matrix, return it to matrix shape
                 phi = np.reshape(phi, (self.__embedding_size, self.__embedding_size))
-                error = 0.0
-                for x, y in training_data:
-                    if self.__kmeans.predict(x) == k:
-                        error += np.square(np.linalg.norm(x*phi - y))
+                error = np.square(np.linalg.norm(clustered_x[k] @ phi - clustered_y[k]))
                 return 1.0/n * error
 
-            result = minimize(fun=f, x0=self.__phi[k])
+            result = minimize(fun=f, x0=self.__phi[k], method='BFGS',
+                              options={'disp': False, 'gtol': tol, 'eps': eps, 'maxiter': max_iter, 'return_all': False,
+                                       'norm': np.inf})
 
             if not result.success:
                 raise TrainingFailureException(result.message)
 
-            self.__phi[k] = result.x
+            self.__phi[k] = np.reshape(result.x, (self.__embedding_size, self.__embedding_size))
 
     def classify(self, unknowns: np.array)->List[str]:
-        # TODO: implement
-        return list()
+        projections = np.zeros(unknowns.shape)
+        labels = list()
+        cluster_labels = self.__kmeans.predict(unknowns)
+
+        # project all unknowns
+        for i, c in enumerate(cluster_labels):
+            projections[i] = unknowns[i] @ self.__phi[c]
+        # find indices of most similar embeddings
+        indexes = np.argmax(cosine_similarity(projections, self.__embeddings), axis=1)
+
+        for index in indexes:
+            labels.append(self.__labels[index])
+        return labels
 
 
 class TrainingFailureException(Exception):
