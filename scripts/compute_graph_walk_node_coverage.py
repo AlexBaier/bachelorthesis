5ea4b9d06d82cbbf2becd43321f5983e97f80d98
Alex Baier <abaier@uni-koblenz.de>
2017-03-13 19:56:28 +0100

    Updated node coverage script.

--- scripts/compute_graph_walk_node_coverage.py
+++ scripts/compute_graph_walk_node_coverage.py
@@ -2,19 +2,16 @@ from algorithm.sentence_gen import SentenceIterator
 
 
 def main():
-    node_id_path = '../data/algorithm_io/item_ids-20161107.txt'
-    sentence_path = '../data/algorithm_io/graphwalk_sentences-20161107-1.txt'
+    node_id_path = '../data/algorithm_io/class_ids-20161107.txt'
+    sentence_path = '../data/algorithm_io/graphwalk_sentences-20161107.txt'
 
     nodes = set()
     with open(node_id_path) as f:
         for l in f:
             nodes.add(l.strip())
 
-    covered_nodes = set()
-    for sentence in SentenceIterator(sentence_path):
-        for word in sentence:
-            if word[0] == 'Q':
-                covered_nodes.add(word)
+    with open(sentence_path) as f:
+        covered_nodes = set(filter(lambda w: w[0] == 'Q', f.read().replace('\n', ' ').split()))
 
     print('coverage: {}/{} => {}%'.format(len(covered_nodes), len(nodes), 100*float(len(covered_nodes))/len(nodes)))
 
