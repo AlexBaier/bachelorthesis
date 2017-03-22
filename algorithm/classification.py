11996f11fbf336e7833548df8525a1ed90b453eb
Alex Baier <abaier@uni-koblenz.de>
2017-03-22 10:28:33 +0100

    Reset "fix" of piecewise linear projection. Small fixes to remove warnings from code.

--- algorithm/classification.py
+++ algorithm/classification.py
@@ -79,7 +79,7 @@ class KRIKNNClassifier(KNNClassifier):
     def classify(self, unknowns: np.array)->List[str]:
         labels = list()
         n = unknowns.shape[0]
-        index_matrix = self.__nearest_neighbors.kneighbors(unknowns, return_distance=False)
+        _, index_matrix = self.__nearest_neighbors.kneighbors(unknowns, return_distance=True)
         logging.log(level=logging.INFO, msg='KRI-kNN: found {}-nearest neighbors for {} unknowns'
                     .format(self.__neighbors, n))
         for i in range(n):
@@ -156,7 +156,7 @@ class LinearProjectionClassifier(ProjectionClassifier):
         y = np.array(y).T
         # calculate similarities between projections and all embeddings
         # get indices of most similar embedding
-        indexes = self.__nearest_neighbors.kneighbors(y, return_distance=False)
+        _, indexes = self.__nearest_neighbors.kneighbors(y, return_distance=True)
 
         for index in indexes:
             labels.append(self.__labels[index[0]])
@@ -170,6 +170,7 @@ class PiecewiseLinearProjectionClassifier(ProjectionClassifier):
         self.__clusters = clusters  # type: int
         self.__embedding_size = embedding_size  # type: int
         self.__embeddings = embeddings
+        self.__kmeans = MiniBatchKMeans(n_clusters=clusters)  # type: MiniBatchKMeans
         self.__labels = labels  # type: List[str]
         self.__nearest_neighbors = NearestNeighbors(
             metric='minkowski',
@@ -181,16 +182,13 @@ class PiecewiseLinearProjectionClassifier(ProjectionClassifier):
 
     def train(self, training_data: List[Tuple[np.array, np.array]], batch_size: int=500, n_iter: int=5):
         self.__nearest_neighbors.fit(self.__embeddings)
+        self.__kmeans.fit(self.__embeddings)
+        logging.log(level=logging.INFO, msg='finished clustering, clusters={}'.format(self.__clusters))
 
         x, y = list(map(np.array, zip(*training_data)))
         del training_data
 
-        k_means = MiniBatchKMeans(n_clusters=self.__clusters)
-        k_means.fit(y - x)
-        logging.log(level=logging.INFO,
-                    msg='finished clustering of subclass-offsets, clusters={}'.format(self.__clusters))
-
-        cluster_labels = k_means.predict(y - x)
+        cluster_labels = self.__kmeans.predict(x)
         clustered_x = [list() for _ in range(self.__clusters)]
         clustered_y = [list() for _ in range(self.__clusters)]
         for i, c in enumerate(cluster_labels):
@@ -200,7 +198,6 @@ class PiecewiseLinearProjectionClassifier(ProjectionClassifier):
         del y
         clustered_x = list(map(np.array, clustered_x))
         clustered_y = list(map(np.array, clustered_y))
-
         logging.log(level=logging.INFO, msg='sorted training samples into clusters')
 
         for c in range(self.__clusters):
@@ -223,21 +220,22 @@ class PiecewiseLinearProjectionClassifier(ProjectionClassifier):
                                 .format(c+1, i+1, n_iter, k+1, len(batches)))
 
     def classify(self, unknowns: np.array)->List[str]:
-        # TODO: replace with a vectorized implementation.
+        projections = np.zeros(unknowns.shape)
         labels = list()
-        n = unknowns.shape[0]
-        for i, unknown in enumerate(unknowns):
-            unknown = unknown.reshape(1, -1)
-            predictions = np.zeros((self.__clusters, self.__embedding_size))
+        cluster_labels = self.__kmeans.predict(unknowns)
 
-            for c in range(self.__clusters):
-                for target in range(self.__embedding_size):
-                    predictions[c][target] = self.__sgd_regressors[c][target].predict(unknown)
+        # project all unknowns
+        for i, c in enumerate(cluster_labels):
+            y = list()
+            for target in range(self.__embedding_size):
+                y.append(self.__sgd_regressors[c][target].predict(unknowns[i].reshape(1, -1)))
+            y = np.array(y).T
+            projections[i] = y
+        # find indices of most similar embeddings
+        _, indexes = self.__nearest_neighbors.kneighbors(projections, return_distance=True)
 
-            distances, neighbor_idx = self.__nearest_neighbors.kneighbors(predictions, return_distance=True)
-            labels.append(self.__labels[neighbor_idx[np.argmin(distances)][0]])
-            logging.log(level=logging.INFO, msg='classification progress: {}/{}'.format(i, n))
-
+        for index in indexes:
+            labels.append(self.__labels[index[0]])
         return labels
 
 
