e664a4ea1c784d1856c738769166aaea55d07f3c
Alex Baier <abaier@uni-koblenz.de>
2017-03-29 12:48:03 +0200

    Removed unused imports.

--- evaluation/statistics.py
+++ evaluation/statistics.py
@@ -1,10 +1,7 @@
 import sqlite3
-
-import logging
-from typing import Callable, Dict, List, Tuple, Set
+from typing import Callable, Dict, List, Tuple
 
 import numpy as np
-import pathos.multiprocessing as mp
 from sklearn.metrics.pairwise import cosine_similarity
 
 from evaluation.data_sample import MultiLabelSample
