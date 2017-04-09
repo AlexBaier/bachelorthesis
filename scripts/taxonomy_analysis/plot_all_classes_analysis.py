8224903b52317b5157a8cb241f1a3cd1497575b8
AlexBaier <abaier@uni-koblenz.de>
2017-04-09 03:02:51 +0200

    Fixed plotting of all class analysis.

--- scripts/taxonomy_analysis/plot_all_classes_analysis.py
+++ scripts/taxonomy_analysis/plot_all_classes_analysis.py
@@ -2,6 +2,8 @@ import json
 import logging
 
 import matplotlib.pyplot as plt
+import numpy as np
+from typing import Dict, List, Set
 
 
 def main():
@@ -10,25 +12,20 @@ def main():
     with open('paths_config.json') as f:
         paths_config = json.load(f)
 
-    with open(paths_config['wikidata labels']) as f:
-        labels = dict([l.strip().split(',') for l in f])
-    logging.log(level=logging.INFO, msg='loaded labels')
-
     with open(paths_config['class analysis']) as f:
         analysis = json.load(f)
-    logging.log(level=logging.INFO, msg='loaded {}'.format(analysis))
+    logging.log(level=logging.INFO, msg='loaded analysis')
 
-    frequency_limit = 20
+    labels_path = paths_config['wikidata labels']
+
+    frequency_limit = 25
     frequency_plots = [
         {'title': 'property frequencies of all classes',
          'path': paths_config['class property frequency'],
-         'key': 'property histogram'},
-        {'title': 'subclass frequencies of all classes',
-         'path': paths_config['class subclass frequency'],
-         'key': 'subclass histogram'}
+         'key': 'property histogram'}
     ]
 
-    count_limit = 40
+    count_limit = 15
     count_plots = [
         {'title': 'property count histogram for all classes',
          'x': 'number of properties',
@@ -44,22 +41,39 @@ def main():
          'key': 'instance count histogram'}
     ]
 
+    labels = dict()  # type: Dict[str, str]
+
+    def add_labels(cids: Set[str]):
+        with open(labels_path) as g:
+            for eid, label in map(lambda l: l.strip().split(',', 1), g):
+                if eid in cids:
+                    labels[eid] = label
+
     for frequency_plot in frequency_plots:
         plt.figure(1)
         plt.clf()
 
-        x, y = map(lambda t: (t[0], int(t[1])), sorted(analysis[frequency_plot['key']].items(), key=lambda i: i[1]))
-        top_labels = [labels[pid] for pid in x[::-1][:frequency_limit]]
-        top_values = y[::-1][:frequency_limit]
-        rest_sum = sum(y[::-1][frequency_limit:])
+        fig, ax = plt.subplots()
 
-        labels = top_labels + ['other properties']
-        values = top_values + [rest_sum]
+        x, y = zip(*map(lambda t: (t[0], int(t[1])),
+                        sorted(analysis[frequency_plot['key']].items(), key=lambda i: int(i[1]))))
+        top_ids = list(x[::-1][:frequency_limit])  # type: List[str]
+        # normalized values
+        top_values = np.array((y[::-1][:frequency_limit])) / np.sum(np.array(y))
 
-        plt.pie(values, labels=labels, startangle=90)
-        plt.axis('equal')
-        plt.title(frequency_plot['title'])
-        plt.legend()
+        if not set(top_ids).issubset(set(labels.keys())):
+            add_labels(set(top_ids).difference(set(labels.keys())))
+
+        top_labels = ['{} ({})'.format(labels[tid], tid) for tid in top_ids]   # type: List[str]
+
+        top_labels = top_labels + ['other properties']  # type: List[str]
+
+        ax.barh(np.arange(frequency_limit), top_values, color='#3f9b0b')
+        ax.set_title(frequency_plot['title'])
+
+        ax.set_yticks(np.arange(frequency_limit))
+        ax.set_yticklabels(top_labels, va='bottom')
+        plt.tight_layout()
 
         plt.savefig(frequency_plot['path'])
         logging.log(logging.INFO, msg='stored frequency plot to {}'.format(frequency_plot['path']))
@@ -68,11 +82,13 @@ def main():
         plt.figure(1)
         plt.clf()
 
-        x, y = zip(*sorted(map(lambda t: (int(t[0], int(1))), analysis[count_plot['key']]), key=lambda i: i[0]))
-        top_x = x[:count_limit]
-        top_y = y[:count_limit]
+        x, y = zip(*map(lambda t: (int(t[0]), int(t[1])),
+                        sorted(analysis[count_plot['key']].items(), key=lambda i: int(i[0]))))
+        top_x = np.array(x[:count_limit])
+        top_y = np.array(y[:count_limit]) / np.sum(np.array(y))
 
-        plt.bar(top_x, top_y, color='#380282')  # color: indigo
+        plt.xlim([-0.5, count_limit])
+        plt.bar(top_x, top_y, color='#2c6fbb', align='center')
         plt.xlabel(count_plot['x'])
         plt.ylabel('number of classes')
         plt.title(count_plot['title'])
