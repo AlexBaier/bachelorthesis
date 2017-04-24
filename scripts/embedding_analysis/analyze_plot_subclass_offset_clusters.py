72f324d4f11ba4324eef9ac1af881f864aaa3392
AlexBaier <abaier@uni-koblenz.de>
2017-04-24 10:47:18 +0200

    Improved plotting of word embeddings.

--- scripts/embedding_analysis/analyze_plot_subclass_offset_clusters.py
+++ scripts/embedding_analysis/analyze_plot_subclass_offset_clusters.py
@@ -8,30 +8,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-
-# copied from https://gist.github.com/adewes/5884820
-def get_random_color(pastel_factor=0.5):
-    return [(x+pastel_factor)/(1.0+pastel_factor) for x in [random.uniform(0, 1.0) for _ in [1, 2, 3]]]
-
-
-# copied from https://gist.github.com/adewes/5884820
-def color_distance(c1, c2):
-    return sum([abs(x[0]-x[1]) for x in zip(c1, c2)])
-
-
-# copied from https://gist.github.com/adewes/5884820
-def generate_new_color(existing_colors, pastel_factor=0.5):
-    max_distance = None
-    best_color = None
-    for i in range(0, 100):
-        color = get_random_color(pastel_factor=pastel_factor)
-        if not existing_colors:
-            return color
-        best_distance = min([color_distance(color, c) for c in existing_colors])
-        if not max_distance or best_distance > max_distance:
-            max_distance = best_distance
-            best_color = color
-    return best_color
+from evaluation.utils import generate_new_color
 
 
 def main():
--- scripts/embedding_analysis/scatter_plot_embeddings.py
+++ scripts/embedding_analysis/scatter_plot_embeddings.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-from evaluation.utils import load_embeddings_and_labels
+from evaluation.utils import load_embeddings_and_labels, generate_new_color
 
 
 def main():
@@ -20,23 +20,27 @@ def main():
     embeddings_path = algorithm_config['triple sentence sgns']['embeddings path']
     plot_output = paths_config['embeddings plot']
 
+    n_clusters = 50
+
     embeddings, _ = load_embeddings_and_labels(embeddings_path)
     logging.log(level=logging.INFO, msg='loaded embeddings')
+
+    ms = KMeans(n_clusters=n_clusters, n_jobs=3)
+    clusters = ms.fit_predict(embeddings)
+    logging.log(level=logging.INFO, msg='executed clustering on embeddings')
 
     pca = PCA(n_components=2)
     pca.fit(embeddings)
     embeddings = pca.transform(embeddings)
     logging.log(level=logging.INFO, msg='executed pca')
 
-    ms = KMeans(n_clusters=9, n_jobs=3)
-    clusters = ms.fit_predict(embeddings)
-    logging.log(level=logging.INFO, msg='executed clustering on 2d offsets')
-
     xs, ys = embeddings.T
 
-    colors = ['#FF4405', '#FFC105', '#C1FF05', '#44FF05', '#FF9F80', '#05FF44', '#FF05C1', '#42D0FF', '#4405FF']
+    colors = []
+    for i in range(n_clusters):
+        colors.append(generate_new_color(colors, pastel_factor=0))
 
-    plt.scatter(xs, ys, s=0.1, edgecolors=[colors[cluster] for cluster in clusters])
+    plt.scatter(xs, ys, s=0.1, color=[colors[cluster] for cluster in clusters])
     logging.log(level=logging.INFO, msg='save plot to {}'.format(plot_output))
     plt.savefig(plot_output)
 
